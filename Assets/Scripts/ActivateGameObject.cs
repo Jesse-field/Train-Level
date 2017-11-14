0b0a80f628e8524d4745f2007cf695a69410c2e3
Jesse-field <jesse.f.designer@gmail.com>
2017-11-14 14:37:49 +1100

    Train Level added

---
+++ Assets/Scripts/ActivateGameObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateGameObject : MonoBehaviour {
+
+	public GameObject triggerToActivate;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnTriggerEnter(Collider Other)
+
+	{
+		
+		triggerToActivate.SetActive (true);
+	}
+}
